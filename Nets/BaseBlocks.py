2b6cc928cedd6f683aca6ff501ef67b14d94b1bc
joempappas <pappas_joe@yahoo.com>
2022-11-11 22:11:17 -0500

    Change SE activation funcions

--- Nets/BaseBlocks.py
+++ Nets/BaseBlocks.py
@@ -31,9 +31,9 @@ class SEBlock(nn.Module):
         self.squeeze = nn.AdaptiveAvgPool2d(1)
         hidden_dim = in_channels // r_factor
         self.excite = nn.Sequential(nn.Conv2d(in_channels, hidden_dim, kernel_size=1),
-                                        nn.SiLU(),
+                                        nn.ReLU(),
                                         nn.Conv2d(hidden_dim, in_channels, kernel_size=1),
-                                        nn.SiLU())
+                                        nn.Sigmoid())
         
     def forward(self, x):
         result = self.squeeze(x)
